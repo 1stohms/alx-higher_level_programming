932ba925e23c83baf6f485e40133842ab7115633
sina <111012499+1stohms@users.noreply.github.com>
2022-11-30 15:26:28 +0100

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_function/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_function/0-positive_or_negative.py
@@ -1,27 +1,9 @@
-#!/usr/bin/env python3
-
-# Created by: Caylee Annett
-# Created on: May 2021
-# This program tells the user if a number is positive or negative
-
-
-def main():
-    # This function tells the user if their number is positive or negative
-
-    # Input
-    print("This program checks if a number is positive or negative.")
-    print("")
-    integer = int(input("Enter an integer: "))
-
-    # Process & Output
-    if integer >= 1:
-        print("{} is a positive number.".format(integer))
-    elif integer <= -1:
-        print("{} is a negative number.".format(integer))
-    else:
-        print("0 is just a zero!")
-    print("\nDone.")
-
-
-if __name__ == "__main__":
-    main()
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+else:
+    print("{} is negative".format(number))
